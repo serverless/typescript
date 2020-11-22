e5545b2df3ebbca8716218240580a129d9f61d85
Frédéric Barthelet <fredericb@theodo.fr>
2020-11-22 16:58:01 +0100

    Add normalizer to transform const to single-value enum

--- plugin.js
+++ plugin.js
@@ -23,12 +23,40 @@ class ConfigSchemaHandlerTypescriptDefinitionsPlugin {
     delete this.schema.properties.provider.properties.s3.additionalProperties.properties.replicationConfiguration
 
     /**
+     * https://github.com/serverless/typescript/issues/4
+     * JSON Schema v6 `const` keyword converted to `enum`
+     */
+    const normalizedSchema = replaceAllConstForEnum(this.schema);
+
+    /**
      * format: false -> improves generation performances
      * ignoreMinAndMaxItems: true -> maxItems: 100 in provider.s3.corsConfiguration definition is generating 100 tuples
      */
-    const compiledDefinitions = await compile(this.schema, 'AWS', { format: false, ignoreMinAndMaxItems: true })
+    const compiledDefinitions = await compile(normalizedSchema, 'AWS', { format: false, ignoreMinAndMaxItems: true })
     fs.writeFileSync('index.d.ts', compiledDefinitions)
   }
+}
+
+function replaceAllConstForEnum(schema) {
+  if ('object' !== typeof schema) {
+    return schema
+  }
+
+  return Object.fromEntries(Object.entries(schema).map(([key, value]) => {
+    if (key === 'const') {
+      return ['enum', [value]]
+    }
+
+    if (Array.isArray(value)) {
+      return [key, value.map(replaceAllConstForEnum)]
+    }
+
+    if ('object' === typeof value && value !== null) {
+      return [key, replaceAllConstForEnum(value)]
+    }
+
+    return [key, value]
+  }))
 }
 
 module.exports = ConfigSchemaHandlerTypescriptDefinitionsPlugin
