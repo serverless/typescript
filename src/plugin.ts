a46a20aef72c039faf05732b10ce27a0bc9c2926
Frédéric Barthelet <fredericb@theodo.fr>
2021-03-09 15:33:51 +0100

    Use deep clone before compiling

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,6 +1,7 @@
 import { compile } from "json-schema-to-typescript";
 import fs from "fs";
 import type { JSONSchema4 } from "json-schema";
+import * as _ from "lodash";
 
 interface Serverless {
   configSchemaHandler: {
@@ -12,7 +13,7 @@ class ConfigSchemaHandlerTypescriptDefinitionsPlugin {
   private schema: JSONSchema4;
 
   constructor(serverless: Serverless) {
-    this.schema = serverless.configSchemaHandler.schema;
+    this.schema = _.cloneDeep(serverless.configSchemaHandler.schema);
   }
 
   commands = {
@@ -28,44 +29,13 @@ class ConfigSchemaHandlerTypescriptDefinitionsPlugin {
 
   async generateSchema() {
     /**
-     * https://github.com/serverless/typescript/issues/4
-     * JSON Schema v6 `const` keyword converted to `enum`
-     */
-    const normalizedSchema = replaceAllConstForEnum(this.schema);
-
-    /**
      * ignoreMinAndMaxItems: true -> maxItems: 100 in provider.s3.corsConfiguration definition is generating 100 tuples
      */
-    const compiledDefinitions = await compile(normalizedSchema, "AWS", {
+    const compiledDefinitions = await compile(this.schema, "AWS", {
       ignoreMinAndMaxItems: true,
-      unreachableDefinitions: true,
     });
     fs.writeFileSync("index.d.ts", compiledDefinitions);
   }
 }
-
-const replaceAllConstForEnum = (schema: JSONSchema4): JSONSchema4 => {
-  if ("object" !== typeof schema) {
-    return schema;
-  }
-
-  return Object.fromEntries(
-    Object.entries(schema).map(([key, value]) => {
-      if (key === "const") {
-        return ["enum", [value]];
-      }
-
-      if (Array.isArray(value)) {
-        return [key, value.map(replaceAllConstForEnum)];
-      }
-
-      if ("object" === typeof value && value !== null) {
-        return [key, replaceAllConstForEnum(value)];
-      }
-
-      return [key, value];
-    })
-  );
-};
 
 module.exports = ConfigSchemaHandlerTypescriptDefinitionsPlugin;
