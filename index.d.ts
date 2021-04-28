ac240e3d995ef0ff0e02e0b332c5487069cf6480
Alexey Lyakhov <justfly1984@gmail.com>
2021-04-28 12:25:04 +0700

    updated dependencies, fixed typescript error in index.d.ts file.

--- index.d.ts
+++ index.d.ts
@@ -1236,6 +1236,7 @@ export interface AWS {
     };
     Resources?: {
       "Fn::Transform"?: {
+        Type: string;
         Name: string;
         Parameters?: {
           [k: string]: unknown;
